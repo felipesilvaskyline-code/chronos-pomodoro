ab2f7dd7d43fde3208800a9c379ffdb21c642b19
felipesilvaskyline-code <felipesilva.skyline@gmail.com>
2026-04-06 17:20:08 -0300

    feat: Containeres de seção

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,38 @@
-import { Heading } from "./components/Heading";
-
 import "./styles/theme.css";
 import "./styles/global.css";
-import { TimerIcon } from "lucide-react";
 
 export function App() {
   return (
     <>
-      <Heading>
-        Chronos-pomodoro
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
+      <div className='container'>
+        <div className='content'>
+          <section>
+            Logo
+          </section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>
+            Menu
+          </section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>
+            Form
+          </section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>
+            Footer
+          </section>
+        </div>
+      </div>
 
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
     </>
   );
 }
