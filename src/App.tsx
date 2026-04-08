b4cda9252e4bceb21c83300c83fc1efb04a5728c
Felipe <felipedasilva23785@gmail.com>
2026-04-08 19:15:17 -0300

    feat: Componente do menu com links de navegação

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Container } from "./components/Container";
-import { Heading } from "./components/Heading";
+import { Menu } from "./components/Menu";
 import { Logo } from "./components/Logo";
 
 import "./styles/theme.css";
 import "./styles/global.css";
+
 
 export function App() {
   return (
@@ -12,7 +13,7 @@ export function App() {
         <Logo />
       </Container>
       <Container>
-        <Heading>Menu</Heading>
+        <Menu />
       </Container>
     </>
   );
--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -7,7 +7,7 @@ export function Logo() {
     <div className={styles.logo}>
       <a className={styles.logoLink} href='#'>
         <TimerIcon />
-        <span>Chronos</span>{" "}
+        <span>Chronos</span>
       </a>
     </div>
   );
