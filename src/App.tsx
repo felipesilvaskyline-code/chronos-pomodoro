e6803a798f07a0b0cffeadd6ea8c61532689cb84
felipesilvaskyline-code <felipesilva.skyline@gmail.com>
2026-04-15 12:06:09 -0300

    feat: Formulário versão inicial

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,38 @@ import { CountDown } from "./components/CountDown";
 import "./styles/theme.css";
 import "./styles/global.css";
 
-
 export function App() {
   return (
     <>
       <Container>
         <Logo />
       </Container>
+
       <Container>
         <Menu />
       </Container>
+
       <Container>
         <CountDown />
+      </Container>
+
+      <Container>
+        <form className="form" action="">
+          <div className="formRow">
+            <label htmlFor="input">task</label>
+            <input id="input" type="text"></input>
+          </div>
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0 </p>
+          </div>
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
